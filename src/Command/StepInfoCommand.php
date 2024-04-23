afa23db1fba6ca596c4ea7435ec2e20306554802
Fabien Potencier <fabien@potencier.org>
2024-04-23 18:11:11 +0200

    Add 21.6: Caching CPU/Memory Intensive Operations

---
+++ src/Command/StepInfoCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+use Symfony\Contracts\Cache\CacheInterface;
+
+#[AsCommand('app:step:info')]
+class StepInfoCommand extends Command
+{
+    public function __construct(
+         private CacheInterface $cache,
+    ) {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $step = $this->cache->get('app.current_step', function ($item) {
+            $process = new Process(['git', 'tag', '-l', '--points-at', 'HEAD']);
+            $process->mustRun();
+            $item->expiresAfter(30);
+
+            return $process->getOutput();
+        });
+        $output->writeln($step);
+
+        return Command::SUCCESS;
+    }
+}
