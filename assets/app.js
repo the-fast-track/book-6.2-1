f914099e4c11586a6e2ab55632089ead2657639d
Fabien Potencier <fabien@potencier.org>
2024-04-23 18:12:55 +0200

    Add 22.2: Leveraging Bootstrap

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
+import 'bootstrap';
+import bsCustomFileInput from 'bs-custom-file-input';
+
+bsCustomFileInput.init();
