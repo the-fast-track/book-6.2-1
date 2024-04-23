bcb885d96e43f97d57cc5be940e68178647d067f
Fabien Potencier <fabien@potencier.org>
2024-04-23 17:51:29 +0200

    Add 14.1: Generating a Form Type

---
+++ src/Form/CommentType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use App\Entity\Conference;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('author')
+            ->add('text')
+            ->add('email')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('photoFilename')
+            ->add('conference', EntityType::class, [
+                'class' => Conference::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
