7b6b6663a2663971c0d704b671fe03ff16bdbb80
Fabien Potencier <fabien@potencier.org>
2024-04-23 18:11:08 +0200

    Add 21.3: Avoiding SQL Requests with ESI

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -32,6 +32,14 @@ class ConferenceController extends AbstractController
         ])->setSharedMaxAge(3600);
     }
 
+    #[Route('/conference_header', name: 'conference_header')]
+    public function conferenceHeader(ConferenceRepository $conferenceRepository): Response
+    {
+        return $this->render('conference/header.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ])->setSharedMaxAge(3600);
+    }
+
     #[Route('/conference/{slug}', name: 'conference')]
     public function show(
         Request $request,
